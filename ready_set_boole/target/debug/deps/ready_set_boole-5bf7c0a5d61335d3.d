425ee82a29a7dc8954aaaf25989d5d7019ad7ac8
whyNot077 <peppertones0123@gmail.com>
2024-08-20 19:52:32 +0900

    ready set boole!

---
+++ ready_set_boole/target/debug/deps/ready_set_boole-5bf7c0a5d61335d3.d
@@ -0,0 +1,5 @@
+/Users/minju/Desktop/ready-set-boole/ready_set_boole/target/debug/deps/libready_set_boole-5bf7c0a5d61335d3.rmeta: src/main.rs
+
+/Users/minju/Desktop/ready-set-boole/ready_set_boole/target/debug/deps/ready_set_boole-5bf7c0a5d61335d3.d: src/main.rs
+
+src/main.rs:
